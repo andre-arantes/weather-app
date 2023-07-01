b8016c59aea9f9e3232e786440522c15cc85dbcc
André <andre2004bh@gmail.com>
2023-07-01 17:51:30 -0300

    added dark mode

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -7,23 +7,24 @@ function Card({ data }) {
     current,
   } = data;
   return (
+    <div>
+      <div className="bg-white p-6 mt-10 dark:bg-[#181A1B] dark:text-white rounded-lg shadow-md min-w-[220px]">
+        <div className="text-center">
+          <span className="block text-xl font-bold dark:text-[#ACBDCD] text-slate-700">{location.name}</span>
+          <span className="text-slate-400 dark:text-[#ACBDCD] text-sm font-medium">
+            {`${location.region}, ${location.country}`}
+          </span>
+        </div>
 
-    <div className="bg-white p-6 mt-10 rounded-lg shadow-md min-w-[220px]">
-      <div className="text-center">
-        <span className="block text-xl font-bold text-slate-700">{location.name}</span>
-        <span className="text-slate-400 text-sm font-medium">
-          {`${location.region}, ${location.country}`}
-        </span>
-      </div>
+        <div className="font-bold text-slate-700 dark:text-[#ACBDCD] flex justify-center mt-4 mb-2">
+          <span className="text-8xl">{current.temp_c}</span>
+          <span className="text-2xl mt-2">ºC</span>
+        </div>
 
-      <div className="font-bold text-slate-700 flex justify-center mt-4 mb-2">
-        <span className="text-8xl">{current.temp_c}</span>
-        <span className="text-2xl mt-2">ºC</span>
-      </div>
-
-      <div className="flex justify-center flex-col items-center">
-        <img src={current.condition.icon} alt="weather icon" />
-        <span className="text-slate-700 font-medium">{current.condition.text}</span>
+        <div className="flex justify-center flex-col items-center">
+          <img src={current.condition.icon} alt="weather icon" />
+          <span className="text-slate-700 dark:text-[#ACBDCD] font-medium">{current.condition.text}</span>
+        </div>
       </div>
     </div>
   );
