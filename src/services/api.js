6d1eb3719c9194e8e7e24f42eb9178482b8f2b12
André <andre2004bh@gmail.com>
2023-07-10 20:50:59 -0300

    fixed

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 const KEY = '6968c5ee02c1453aa88183017230107';
 
 const fetchData = async (city) => {
-  const url = `http://api.weatherapi.com/v1/current.json?key=${KEY}&q=${city}&aqi=no`;
+  const url = `https://api.weatherapi.com/v1/current.json?key=${KEY}&q=${city}&aqi=no`;
 
   const fetchResponse = await fetch(url);
   const data = await fetchResponse.json();
